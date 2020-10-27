176e27dc353fe414071c79b2ccf26a6298562f02
JEAN COSTA <00016131@fepi.edu.br>
2020-10-26 23:44:10 -0300

    AERO API – ATIVIDADE 1: CRIAÇÃO DO PROJETO, CONFIGURAÇÃO DO SWAGGER E CRIAÇÃO DAS ROTAS

---
+++ AeroApp/Model/Reserva.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AeroApp.Model
+{
+    public class Reserva
+    {
+        public int Id { get; set; }
+
+        public int VooId { get; set; }
+
+        public String Documento { get; set; }
+
+        public int Poltrona { get; set; }
+
+    }
+}
